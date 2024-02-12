6310c20b3cc72fd43b91aeaf1f51a7cf7e29e4ac
thjle03 <159188736+thjle03@users.noreply.github.com>
2024-02-12 16:17:05 -0300

    Update registrointeligente.sol

--- registrointeligente.sol
+++ registrointeligente.sol
@@ -18,3 +18,27 @@ contract RegistroInteligente {
         return registros[msg.sender];
     }
 }
+// truffle-config.js (o truffle.js)
+
+module.exports = {
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*"
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/your_infura_project_id");
+      },
+      network_id: 3,
+      gas: 5500000
+    },
+    // Otras redes aquí...
+  },
+  compilers: {
+    solc: {
+      version: "^0.8.0" // La versión de Solidity que estás utilizando en tu contrato
+    }
+  }
+};
